a5eb362b238ad9345e4459142c42001636a36699
ameWex <bilousvladw@gmail.com>
2023-11-20 13:29:05 +0100

    spotify

--- src/route/index.js
+++ src/route/index.js
@@ -2,301 +2,294 @@
 const express = require('express')
 // Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
 const router = express.Router()
-// const User = require('./user.js');
 
-// ================================================================
+class Track {
 
-class Product {
   static #list = []
 
-  static #count = 0
-
-  constructor(
-    img,
-    title,
-    description,
-    category,
-    price,
-    amount = 0,
-  ) {
-    this.id = ++Product.#count // Генеруємо унікальний id для товару
-    this.img = img
-    this.title = title
-    this.description = description
-    this.category = category
-    this.price = price
-    this.amount = amount
+  constructor(name, author, image) {
+    this.id = Math.floor(1000 + Math.random() * 9000)
+    this.name = name
+    this.author = author
+    this.image = image
   }
 
-  static add = (...data) => {
-    const newProduct = new Product(...data)
-
-    this.#list.push(newProduct)
+  static create(name, author, image) {
+    const newTrack = new Track(name, author, image)
+    this.#list.push(newTrack)
+    return newTrack
   }
 
-  static getList = () => {
-    return this.#list
+  static getList() {
+    return this.#list.reverse()
   }
 
   static getById = (id) => {
-    // console.log('Searching for product with id:', id)
-    return this.#list.find((product) => product.id === id)
-  }
-
-  static getRandomList = (id) => {
-    // Фільтруємо товари, щоб вилучити той, з яким порівнюємо id
-    const filteredList = this.#list.filter(
-      (product) => product.id !== id,
-    )
-
-    // Відсортовуємо за допомогою Math.random() та перемашаємо масив
-    const shuffledList = filteredList.sort(
-      () => Math.random() - 0.5,
-    )
-
-    // Повертаємо перші 3 елементи з перемішаного масиву
-    return shuffledList.slice(0, 3)
+    return this.#list.find((track) => track.id === id)
   }
 }
 
-Product.add(
-  '../../img/image-616.jpg',
-  `Комп'ютер Artline Gaming (X43v31), AMD Ryzen 5 3600/`,
-  `AMD Ryzen 5 3600 (3.6 - 4.2 ГГц) / RAM 15 ГБ / HDD 1 ТБ + SSD 480 ГБ / nVidia GeForce RTX 3050, 8 ГБ / без ОД / LAN / без ОС`,
-  [
-    { id: 1, text: 'Готовий до відправки' },
-    { id: 2, text: 'Топ продажів' },
-  ],
-  27000,
-  10,
+Track.create(
+  '美少女無罪♡パイレーツ',
+  '宝鐘マリン',
+  'https://i.scdn.co/image/ab67616d0000b273ad7a27fdc6b15c24bb12a1d1',
 )
 
-Product.add(
-  '../../img/image-617.jpg',
-  `Комп'ютер COBRA Advanced (I11F.8.H1S2.15T.13356) Intel`,
-  `Intel Core i3-10100F (3.6 - 4.3 ГГц) / RAM 8 ГБ / HDD 1 ТБ + SSD 240 ГБ / GeForce GTX 1050 Ti, 4 ГБ / без ОД / LAN / Linux`,
-  [
-    { id: 1, text: 'Готовий до відправки' },
-    { id: 2, text: 'Топ продажів' },
-  ],
-  17000,
-  7,
+Track.create(
+  '勇者',
+  'YOASOBI',
+  'https://i.scdn.co/image/ab67616d0000b273a9f9b6f07b43009f5b0216dc',
 )
 
-Product.add(
-  '../../img/image-618.jpg',
-  `Комп'ютер ARTLINE Gaming by ASUS TUF v119 (TUFv119)`,
-  `Intel Core i9-13900KF (3.0 - 5.8 ГГц) / RAM 64 ГБ / SSD 2 ТБ (2 x 1 ТБ) / nVidia GeForce RTX 4070 Ti, 12 ГБ / без ОД / LAN / Wi-Fi / Bluetooth / без ОС`,
-  [
-    { id: 1, text: 'Готовий до відправки' },
-    { id: 2, text: 'Топ продажів' },
-  ],
-  113109,
-  50,
+Track.create(
+  'CRAZY NOISY BIZZARE TOWN',
+  'THE DU',
+  'https://i.scdn.co/image/ab67616d0000b2739d956d70e6b09cd3452f4a59',
 )
+
+Track.create(
+  'MUKANJYO',
+  'Survive Said The Prophet',
+  'https://i.scdn.co/image/ab67616d0000b27366692ec3a0f5437132315850',
+)
+
+Track.create(
+  'Ambiguous',
+  'GARNiDELiA',
+  'https://i.scdn.co/image/ab67616d0000b2738b39373afe41803b83e4fa44',
+)
+
+Track.create(
+  'サクリファイス',
+  '9mm Parabellum Bullet',
+  'https://i.scdn.co/image/ab67616d0000b273035b5e090310fa10b00709d5',
+)
+
+Track.create(
+  'インフェルノ',
+  '9mm Parabellum Bullet',
+  'https://i.scdn.co/image/ab67616d0000b2732d5f2b2b0a1314b0120292c3',
+)
+
+console.log(Track.getList())
+
+class Playlist {
+
+  static #list = []
+
+  constructor(name) {
+    this.id = Math.floor(1000 + Math.random() * 9000)
+    this.name = name
+    this.tracks = []
+    this.image = 'https://picsum.photos/164/164'
+  }
+
+  static create(name) {
+    const newPlaylist = new Playlist(name)
+    this.#list.push(newPlaylist)
+    return newPlaylist
+  }
+
+  static getList() {
+    return this.#list.reverse()
+  }
+
+  static makeMix(playlist) {
+    const allTracks = Track.getList()
+
+    let randomTracks = allTracks
+      .sort(() => 0.5 - Math.random())
+      .slice(0, 3)
+
+    playlist.tracks.push(...randomTracks)
+  }
+
+  static getById(id) {
+    return (
+      Playlist.#list.find(
+        (playlist) => playlist.id === id,
+      ) || null
+    )
+  }
+
+  deleteTrackById(trackId) {
+    this.tracks = this.tracks.filter(
+      (track) => track(id) !== trackId,
+    )
+  }
+}
+
 // ================================================================
 
 // ↙️ тут вводимо шлях (PATH) до сторінки
-router.get('/', function (req, res) {
-  // res.render генерує нам HTML сторінку
+router.get(
+  '/'
+  // 'spotify-choose'
+  , function (req, res) {
+    // res.render генерує нам HTML сторінку
 
-  res.render('purchase-index', {
-    style: 'purchase-index',
+    res.render('spotify-choose', {
+
+      style: 'spotify-choose',
+
+      data: {},
+    })
+  })
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+// router.get('/', function (req, res) {
+//   // res.render генерує нам HTML сторінку
+
+//   res.render('spotify-my-lib', {
+
+//     style: 'spotify-my-lib',
+
+//     data: {},
+//   })
+// })
+
+// ================================================================
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.get('/spotify-create', function (req, res) {
+
+  const isMix = !!req.query.isMix
+
+  console.log(isMix)
+
+  res.render('spotify-create', {
+
+    style: 'spotify-create',
 
     data: {
-      list: Product.getList(),
+      isMix,
     },
   })
 })
 
 // ================================================================
 
-router.get('/purchase-index', function (req, res) {
-  // res.render генерує нам HTML сторінку
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.post('/spotify-create', function (req, res) {
+  console.log(req.body, req.query)
 
-  // ↙️ cюди вводимо назву файлу з сontainer
-  res.render('purchase-index', {
-    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
-    style: 'purchase-index',
-    component: ['heading', 'purchase-product', 'button'],
+  const isMix = !!req.query.isMix
+  const name = req.body.name
 
-    title: 'Товари',
-    description: `Комп'ютери та ноутбуки/Комп'ютери, неттопи, моноблоки`,
-
-    data: {
-      title: 'Товари',
-      subtitle:
-        "Комп'ютери та ноутбуки/Комп'ютери, неттопи, моноблоки",
-
-      products: Product.getList(),
-      list: Product.getRandomList(id),
-      product: Product.getById(id),
-    },
-  })
-
-  console.log(Product.getRandomList(id))
-  // ↑↑ сюди вводимо JSON дані
-})
-
-// ================================================================
-
-router.get('/purchase-product', function (req, res) {
-  // res.render генерує нам HTML сторінку
-  const id = Number(req.query.id)
-
-  // ↙️ cюди вводимо назву файлу з сontainer
-  res.render('purchase-product', {
-    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
-    style: 'purchase-product',
-
-    data: {
-      list: Product.getRandomList(id),
-      product: Product.getById(id),
-    },
-  })
-
-  // ↑↑ сюди вводимо JSON дані
-})
-
-// ================================================================
-
-router.post('/product-create', function (req, res) {
-  // res.render генерує нам HTML сторінку
-  const { prodName, price, description } = req.body
-
-  const product = new Product(prodName, price, description)
-
-  Product.add(product)
-
-  console.log(Product.getList())
-
-  // ↙️ cюди вводимо назву файлу з сontainer
-  res.render('alert', {
-    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
-    style: 'alert',
-    info: 'Товар успішно додано',
-  })
-  // ↑↑ сюди вводимо JSON дані
-})
-
-// ================================================================
-
-router.get('/product-list', function (req, res) {
-  // res.render генерує нам HTML сторінку
-  const list = Product.getList()
-
-  console.log(list)
-  // ↙️ cюди вводимо назву файлу з сontainer
-  res.render('product-list', {
-    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
-    style: 'product-list',
-
-    data: {
-      products: {
-        list,
-        isEmpty: list.length === 0,
-      },
-    },
-  })
-  // ↑↑ сюди вводимо JSON дані
-})
-
-// ================================================================
-
-router.get('/product-edit', function (req, res) {
-  // res.render генерує нам HTML сторінку
-
-  const { id } = req.query
-
-  const product = Product.getById(Number(id))
-
-  // console.log(product)
-
-  if (product) {
-    // ↙️ cюди вводимо назву файлу з сontainer
-    return res.render('product-edit', {
-      // вказуємо назву папки контейнера, в якій знаходяться наші стилі
-      style: 'product-edit',
+  if (!name) {
+    return res.render('alert', {
+      style: 'alert',
 
       data: {
-        prodName: product.prodName,
-        price: product.price,
-        id: product.id,
-        description: product.description,
+        message: 'Помилка',
+        info: 'Введіть назву плейліста',
+        link: isMix
+          ? '/spotify-create?isMix=true'
+          : '/spotify-create',
       },
     })
-  } else {
+  }
+
+  const playlist = Playlist.create(name)
+
+  if (isMix) {
+    Playlist.makeMix(playlist)
+  }
+
+  console.log(playlist)
+
+  res.render('spotify-playlist', {
+    style: 'spotify-playlist',
+
+    data: {
+      playlistId: playlist.id,
+      tracks: playlist.tracks,
+      name: playlist.name,
+      image: playlist.image,
+    },
+  })
+})
+
+// ================================================================
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.get('/spotify-playlist', function (req, res) {
+  const id = Number(req.query.id)
+
+  const playlist = Playlist.getById(id)
+
+  if (!playlist) {
+    return res.render('spotify-playlist', {
+      style: 'spotify-playlist',
+
+      data: {
+        message: 'Помилка',
+        info: 'Такого плейліста не знайдено',
+        link: '/',
+      },
+    })
+  }
+
+  playlist.deleteTrackById(trackId)
+
+  res.render('spotify-playlist', {
+    style: 'spotify-playlist',
+
+    data: {
+      playlistId: playlist.id,
+      tracks: playlist.tracks,
+      name: playlist.name,
+    },
+  })
+})
+
+router.get('/spotify-track-delete', function (req, res) {
+  const playlistId = Number(req.query.playlistId)
+  const trackId = Number(req.query.trackId)
+
+  const playlist = Playlist.getById(playlistId)
+
+  if (!playlist) {
     return res.render('alert', {
-      // вказуємо назву папки контейнера, в якій знаходяться наші стилі
       style: 'alert',
-      info: 'Продукту за таким ID не знайдено',
-      link: '/product-edit  ',
+
+      data: {
+        message: 'Помилка',
+        info: 'Такого плейліста не знайдено',
+        link: `/spotify-playlist?id=${playlistId}`,
+      },
     })
   }
-})
-// ↑↑ сюди вводимо JSON дані
 
-// ================================================================
+  playlist.deleteTrackById(trackId)
 
-router.post('/product-edit', function (req, res) {
-  // res.render генерує нам HTML сторінку
-  const { id, prodName, price, description } = req.body
+  res.render('spotify-playlist', {
+    style: 'spotify-playlist',
 
-  const product = Product.updateById(Number(id), {
-    prodName,
-    price,
-    description,
+    data: {
+      playlistId: playlist.id,
+      tracks: playlist.tracks,
+      name: playlist.name,
+    },
   })
-
-  console.log(id)
-  console.log(product)
-
-  if (product) {
-    // ↙️ cюди вводимо назву файлу з сontainer
-    res.render('alert', {
-      // вказуємо назву папки контейнера, в якій знаходяться наші стилі
-      style: 'alert',
-      info: 'Інформація про товар оновлена',
-    })
-  } else {
-    // ↙️ cюди вводимо назву файлу з сontainer
-    res.render('alert', {
-      // вказуємо назву папки контейнера, в якій знаходяться наші стилі
-      style: 'alert',
-      info: 'Сталася помилка',
-      link: '/product-edit',
-    })
-  }
-  // ↑↑ сюди вводимо JSON дані
 })
 
 // ================================================================
 
-router.get('/product-delete', function (req, res) {
-  // res.render генерує нам HTML сторінку
-  const { id } = req.query
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.get('/spotify-playlist-add', function (req, res) {
+  const id = Number(req.playlistId)
 
-  Product.deleteById(Number(id))
+  const playlist = Playlist.getById(id)
 
-  // ↙️ cюди вводимо назву файлу з сontainer
-  res.render('alert', {
-    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
-    style: 'alert',
-    info: 'Товар видалений',
-    link: '/product-create',
+  res.render('spotify-playlist', {
+    style: 'spotify-playlist',
+
+    data: {
+      playlistId: playlist.id,
+      tracks: Track.getList(),
+    },
   })
-  // ↑↑ сюди вводимо JSON дані
-})
-
-router.get('/product-create', function (req, res) {
-  // res.render генерує нам HTML сторінку
-  const list = Product.getList()
-  // ↙️ cюди вводимо назву файлу з сontainer
-  res.render('product-create', {
-    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
-    style: 'product-create',
-  })
-  // ↑↑ сюди вводимо JSON дані
 })
 
 // Підключаємо роутер до бек-енду
---
+++ src/route/purchase.js
@@ -0,0 +1,282 @@
+
+class Product {
+    static #list = []
+
+    static #count = 0
+
+    constructor(
+        img,
+        title,
+        description,
+        category,
+        price,
+        amount = 0,
+    ) {
+        this.id = ++Product.#count // Генеруємо унікальний id для товару
+        this.img = img
+        this.title = title
+        this.description = description
+        this.category = category
+        this.price = price
+        this.amount = amount
+    }
+
+    static add = (...data) => {
+        const newProduct = new Product(...data)
+
+        this.#list.push(newProduct)
+    }
+
+    static getList = () => {
+        return this.#list
+    }
+
+    static getById = (id) => {
+        // console.log('Searching for product with id:', id)
+        return this.#list.find((product) => product.id === id)
+    }
+
+    static getRandomList = (id) => {
+        // Фільтруємо товари, щоб вилучити той, з яким порівнюємо id
+        const filteredList = this.#list.filter(
+            (product) => product.id !== id,
+        )
+
+        // Відсортовуємо за допомогою Math.random() та перемашаємо масив
+        const shuffledList = filteredList.sort(
+            () => Math.random() - 0.5,
+        )
+
+        // Повертаємо перші 3 елементи з перемішаного масиву
+        return shuffledList.slice(0, 3)
+    }
+}
+
+Product.add(
+    '../../img/image-616.jpg',
+    `Комп'ютер Artline Gaming (X43v31), AMD Ryzen 5 3600/`,
+    `AMD Ryzen 5 3600 (3.6 - 4.2 ГГц) / RAM 15 ГБ / HDD 1 ТБ + SSD 480 ГБ / nVidia GeForce RTX 3050, 8 ГБ / без ОД / LAN / без ОС`,
+    [
+        { id: 1, text: 'Готовий до відправки' },
+        { id: 2, text: 'Топ продажів' },
+    ],
+    27000,
+    10,
+)
+
+Product.add(
+    '../../img/image-617.jpg',
+    `Комп'ютер COBRA Advanced (I11F.8.H1S2.15T.13356) Intel`,
+    `Intel Core i3-10100F (3.6 - 4.3 ГГц) / RAM 8 ГБ / HDD 1 ТБ + SSD 240 ГБ / GeForce GTX 1050 Ti, 4 ГБ / без ОД / LAN / Linux`,
+    [
+        { id: 1, text: 'Готовий до відправки' },
+        { id: 2, text: 'Топ продажів' },
+    ],
+    17000,
+    7,
+)
+
+Product.add(
+    '../../img/image-618.jpg',
+    `Комп'ютер ARTLINE Gaming by ASUS TUF v119 (TUFv119)`,
+    `Intel Core i9-13900KF (3.0 - 5.8 ГГц) / RAM 64 ГБ / SSD 2 ТБ (2 x 1 ТБ) / nVidia GeForce RTX 4070 Ti, 12 ГБ / без ОД / LAN / Wi-Fi / Bluetooth / без ОС`,
+    [
+        { id: 1, text: 'Готовий до відправки' },
+        { id: 2, text: 'Топ продажів' },
+    ],
+    113109,
+    50,
+)
+
+// ================================================================
+
+router.get('/purchase-index', function (req, res) {
+    // res.render генерує нам HTML сторінку
+
+    // ↙️ cюди вводимо назву файлу з сontainer
+    res.render('purchase-index', {
+        // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+        style: 'purchase-index',
+        component: ['heading', 'purchase-product', 'button'],
+
+        title: 'Товари',
+        description: `Комп'ютери та ноутбуки/Комп'ютери, неттопи, моноблоки`,
+
+        data: {
+            title: 'Товари',
+            subtitle:
+                "Комп'ютери та ноутбуки/Комп'ютери, неттопи, моноблоки",
+
+            products: Product.getList(),
+            list: Product.getRandomList(id),
+            product: Product.getById(id),
+        },
+    })
+
+    console.log(Product.getRandomList(id))
+    // ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
+router.get('/purchase-product', function (req, res) {
+    // res.render генерує нам HTML сторінку
+    const id = Number(req.query.id)
+
+    // ↙️ cюди вводимо назву файлу з сontainer
+    res.render('purchase-product', {
+        // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+        style: 'purchase-product',
+
+        data: {
+            list: Product.getRandomList(id),
+            product: Product.getById(id),
+        },
+    })
+
+    // ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
+router.post('/product-create', function (req, res) {
+    // res.render генерує нам HTML сторінку
+    const { prodName, price, description } = req.body
+
+    const product = new Product(prodName, price, description)
+
+    Product.add(product)
+
+    console.log(Product.getList())
+
+    // ↙️ cюди вводимо назву файлу з сontainer
+    res.render('alert', {
+        // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+        style: 'alert',
+        info: 'Товар успішно додано',
+    })
+    // ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
+router.get('/product-list', function (req, res) {
+    // res.render генерує нам HTML сторінку
+    const list = Product.getList()
+
+    console.log(list)
+    // ↙️ cюди вводимо назву файлу з сontainer
+    res.render('product-list', {
+        // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+        style: 'product-list',
+
+        data: {
+            products: {
+                list,
+                isEmpty: list.length === 0,
+            },
+        },
+    })
+    // ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
+router.get('/product-edit', function (req, res) {
+    // res.render генерує нам HTML сторінку
+
+    const { id } = req.query
+
+    const product = Product.getById(Number(id))
+
+    // console.log(product)
+
+    if (product) {
+        // ↙️ cюди вводимо назву файлу з сontainer
+        return res.render('product-edit', {
+            // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+            style: 'product-edit',
+
+            data: {
+                prodName: product.prodName,
+                price: product.price,
+                id: product.id,
+                description: product.description,
+            },
+        })
+    } else {
+        return res.render('alert', {
+            // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+            style: 'alert',
+            info: 'Продукту за таким ID не знайдено',
+            link: '/product-edit  ',
+        })
+    }
+})
+// ↑↑ сюди вводимо JSON дані
+
+// ================================================================
+
+router.post('/product-edit', function (req, res) {
+    // res.render генерує нам HTML сторінку
+    const { id, prodName, price, description } = req.body
+
+    const product = Product.updateById(Number(id), {
+        prodName,
+        price,
+        description,
+    })
+
+    console.log(id)
+    console.log(product)
+
+    if (product) {
+        // ↙️ cюди вводимо назву файлу з сontainer
+        res.render('alert', {
+            // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+            style: 'alert',
+            info: 'Інформація про товар оновлена',
+        })
+    } else {
+        // ↙️ cюди вводимо назву файлу з сontainer
+        res.render('alert', {
+            // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+            style: 'alert',
+            info: 'Сталася помилка',
+            link: '/product-edit',
+        })
+    }
+    // ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
+router.get('/product-delete', function (req, res) {
+    // res.render генерує нам HTML сторінку
+    const { id } = req.query
+
+    Product.deleteById(Number(id))
+
+    // ↙️ cюди вводимо назву файлу з сontainer
+    res.render('alert', {
+        // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+        style: 'alert',
+        info: 'Товар видалений',
+        link: '/product-create',
+    })
+    // ↑↑ сюди вводимо JSON дані
+})
+
+router.get('/product-create', function (req, res) {
+    // res.render генерує нам HTML сторінку
+    const list = Product.getList()
+    // ↙️ cюди вводимо назву файлу з сontainer
+    res.render('product-create', {
+        // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+        style: 'product-create',
+    })
+    // ↑↑ сюди вводимо JSON дані
+})
+// ================================================================
+
+module.exports = User;
